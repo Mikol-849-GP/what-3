a2737ff9d370d66fa541e5e991c7e3d236ded7e5
Mikol <mikokondonikolau@interia.pl>
2025-11-07 17:02:58 +0100

    Update what.js

--- what.js
+++ what.js
@@ -1,5 +1,13 @@
 function poleProstokąta() {
-  prompt("Dawaj bok 1");
-  prompt("Dawaj bok 2")
+  var b1 = Number(prompt("Dawaj bok 1"))
+  var b2 = Number(prompt("Dawaj bok 2"))
+  var p = b1 * b2
+  if (b1 == b2) {
+    alert("To jest kwadrat")
+    alert("Pole kwadratu to:" `p`)
+  }
+  else {
+    alert("Pole prostokąta to:" `p`)
+  }
 }
 poleProstokąta()
